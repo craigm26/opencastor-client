e8d2c7e6b06c2079ed135277bb0e3f63ce3b915c
Craig Merry <craigm26@gmail.com>
2026-03-29 19:14:00 -0700

    refactor: RobotRepository + MissionRepository data layer (#55)
    * fix: contribute commands via R2RAM subcollection (closes #48)
    
    * refactor: RobotRepository + MissionRepository data layer (closes #47)

---
+++ lib/data/repositories/repositories.dart
@@ -0,0 +1,10 @@
+/// Barrel export for all repository interfaces and providers.
+library;
+
+export 'consent_repository.dart';
+export 'consent_repository_provider.dart';
+export 'firestore_mission_repository.dart';
+export 'mission_repository.dart';
+export 'mission_repository_provider.dart';
+export 'robot_repository.dart';
+export 'robot_repository_provider.dart';
